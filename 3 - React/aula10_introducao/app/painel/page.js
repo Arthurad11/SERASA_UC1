221d3e41b425950cee3bf2b552af33edd2c18088
Arthurad11 <tut798068@gmail.com>
2025-03-12 13:32:01 -0300

    aula 11.4

---
+++ 3 - React/aula10_introducao/app/painel/page.js
@@ -0,0 +1,77 @@
+'use client'
+import { useState } from "react";
+
+import "./painel.css"
+
+function Painel() {
+
+    const [ user, setUser ] = useState("")
+    const [ pass, setPass] = useState("") 
+
+    const [ error, setError ] = useState(false)
+    const [ sucess, setSucess ] = useState(false)
+
+    function login(){
+        if (user == "arthur" && pass == "12345"){
+            setError(false)
+            setSucess(true)
+        } else if (user == "" && pass == "") {
+            setError(false)
+            setSucess(false)
+        } else {
+            setError(true)
+            setSucess(false)
+        }
+    }
+ 
+
+    return ( 
+        <div>
+            <h1> Painel administrativo</h1>
+            
+            <label>
+                Digite o usuário:
+                <input onChange={ (e)=> setUser(e.target.value) } />
+            </label>
+
+            <br/>
+
+            <label>
+                Digite a senha:
+                <input type="password" onChange={ (e)=> setPass(e.target.value) }/>
+            </label>
+
+            <br/>
+
+            <button onClick={ ()=> login()}>Entrar</button>
+
+            {
+                error == true ? 
+                    <div className="error"> 
+                        <p>Usuário ou senha incorretos</p>
+                    </div> 
+                :
+                    <div></div>
+                
+            }
+
+            {
+                sucess == true ? 
+                    <div className="sucess"> 
+                        <p>Logado com sucesso!</p>
+                    </div> 
+                :
+                    <div></div>
+            }
+
+
+
+            
+
+            <br/>
+
+        </div>
+    );
+}
+
+export default Painel;
