8daa5b7fd955875921ac3246b5f67390b1564edb
Arthurad11 <tut798068@gmail.com>
2025-04-08 13:47:07 -0300

    POST, DELETE, PUT api

--- 5 - backend/backend/app/page.js
+++ 5 - backend/backend/app/page.js
@@ -1,8 +1,7 @@
 "use client";
-import axios from "axios";
 
-import Image from "next/image";
-import styles from "./page.module.css";
+import axios from "axios";
+import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -12,12 +11,19 @@ export default function Home() {
     const [preco, alteraPreco] = useState([]);
     const [quantidade, alteraQuantidade] = useState([]);
 
+	const [editando, alteraEditando] = useState(0);
+	const [pesquisa, alteraPesquisa] = useState("");
+
 	async function buscaTodos() {
-		const response = await axios.get("http://localhost:3000/api/produtos");
+		const response = await axios.get("http://localhost:3000/api/produtos/");
 		alteraProdutos(response.data);
 	}
 
-	async function buscaPorId() {}
+	async function buscaPorId( id ) {
+		const response = await axios.get("http://localhost:3000/api/produtos/"+id)
+		alteraProdutos( response.data )
+	}
+
 	function buscaPorNome() {}
 
 	async function insereProduto(e) {
@@ -30,15 +36,39 @@ export default function Home() {
             quantidade: quantidade
         }
 
-        const response = await axios.post("http://localhost:3000/api/produtos", obj)
+        const response = await axios.post("http://localhost:3000/api/produtos/", obj)
         console.log(response.data)
 
         buscaTodos()
+
+		alteraNome("")
+		alteraPreco("")
+		alteraQuantidade("")
     }
 	
     
-    function atualizaProdutos() {}
-	function removeProdutos() {}
+    async function atualizaProdutos() {
+		const obj ={
+			nome: nome,
+			preco: preco,
+			quantidade: quantidade
+		}
+
+		const response = await axios.put("http://localhost:3000/api/produtos/"+editando, obj)
+
+		buscaTodos()
+
+		alteraEditando(0)
+
+		alteraNome("")
+		alteraPreco("")
+		alteraQuantidade("")
+	}
+	
+	async function removeProdutos(id) {
+		await axios.delete("http://localhost:3000/api/produtos/"+id)
+		buscaTodos()
+	}
 
     function formataData( valor ){
         let data = valor.split("T")[0]
@@ -54,6 +84,25 @@ export default function Home() {
 
         return data + " às " + hora
     }
+
+	function montaEdicao( produto ) {
+		alteraEditando( produto.id )
+
+		alteraNome( produto.nome )
+		alteraPreco( produto.preco )
+		alteraQuantidade( produto.quantidade )
+	}
+
+	function enviaFormulario(e) {
+		e.preventDefault()
+
+		if(editando == 0){
+			insereProduto()
+		} else {
+			atualizaProdutos()
+		}
+	}
+
 
 	useEffect(() => {
 		buscaTodos()
@@ -84,10 +133,17 @@ export default function Home() {
                 tr:nth-child(even) {
                     background-color: #f2f2f2;
                 }
-
+				
+				tr{
+				cursor: pointer;
+			}
                 tr:hover {
                     background-color: #ddd;
                 }
+			
+				button {
+					cursor: pointer;
+				}
                 `}
 			</style>
 
@@ -97,27 +153,39 @@ export default function Home() {
 			<button>Cadastro</button>
 
 			<hr />
+
+			<p>Busca de produtos. Digite o ID</p>
+			<input type="text" onChange={ (e)=> alteraPesquisa(e.target.value)}/>
+			<button onClick={ ()=> buscaPorId(pesquisa)}>Pesquisar</button>
+
 			<h2>Listagem</h2>
 
 			{produtos.length > 0 ? (
 				<table>
-					<tr>
-						<td>ID</td>
-						<td>Nome</td>
-						<td>Preço</td>
-						<td>Quantidade</td>
-						<td>Registro</td>
-					</tr>
-
-					{produtos.map((i) => (
+					<thead>
 						<tr>
-							<td>{i.id}</td>
-							<td>{i.nome}</td>
-							<td>R$ {i.preco.toFixed(2)}</td>
-							<td>{i.quantidade}</td>
-							<td>{formataData(i.registro)}</td>
+							<td>ID</td>
+							<td>Nome</td>
+							<td>Preço</td>
+							<td>Quantidade</td>
+							<td>Registro</td>
 						</tr>
-					))}
+					</thead>
+
+					<tbody>
+						{produtos.map((i) => (
+							<tr>
+								<td>{i.id}</td>
+								<td>{i.nome}</td>
+								<td>R$ {i.preco.toFixed(2)}</td>
+								<td>{i.quantidade}</td>
+								<td>{formataData(i.registro)}</td>
+								<td><button onClick={ () => redirect("/produto/"+i.id)}>Ver</button>
+								<button onClick={ () => montaEdicao(i)} >Editar</button>
+								<button onClick={ () => removeProdutos(i.id)}>Remover</button></td>
+							</tr>
+						))}
+					</tbody>
 				</table>
 			) : (
 				<p>Carregando...</p>
@@ -129,24 +197,31 @@ export default function Home() {
 
 			<hr />
 
-			<form onSubmit={ (e)=> insereProduto(e)}>
+			<form onSubmit={ (e)=> enviaFormulario(e)}>
 				<label>
 					Digite o nome do produto: <br />
-					<input onChange={ (e) => alteraNome(e.target.value)}/>
+					<input onChange={ (e) => alteraNome(e.target.value)} value={nome}/>
 				</label>
 				<br />
 				<label>
 					Digite o preço do produto: <br />
-					<input onChange={ (e) => alteraPreco(e.target.value)}/>
+					<input onChange={ (e) => alteraPreco(e.target.value)} value={preco}/>
 				</label>
 				<br />
 				<label>
 					Digite a quantidade do produto: <br />
-					<input onChange={ (e) => alteraQuantidade(e.target.value)}/>
+					<input onChange={ (e) => alteraQuantidade(e.target.value)} value={quantidade}/>
 				</label>
 				<br />
 				<button>Salvar</button>
 			</form>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
 		</div>
 	);
 }
